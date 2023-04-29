61d4fa2333c207090050d703b542bff848ca9235
Alex Kuchutkov <kashim007@mail.ru>
2023-04-29 16:18:28 +0300

    Commit for work part 2 finish block skills

--- src/section/about me/AboutMe.tsx
+++ src/section/about me/AboutMe.tsx
@@ -2,36 +2,39 @@ import * as React from 'react';
 import containerStyle from "../../common/container/Container.module.css";
 import aboutMeStyle from './AboutMe.module.scss'
 import {SectionTItle} from "../../common/components/title/SectionTItle";
+import {AbloutMeBlock} from "../../common/components/aboutMeBlock/AbloutMeBlock";
 
 type Props = {};
 export const AboutMe = (props: Props) => {
-    return (
-        <section className={aboutMeStyle.aboutMe}>
-            <div className={containerStyle.container}>
-                <SectionTItle sectionTitle={'About me'} header={'Know me more'}/>
-                <div className={aboutMeStyle.wrapper}>
-                    <div className={aboutMeStyle.wrapper_description}>
-                        <h2>Hi, I'm <span>Alexandr Kuchutkou</span></h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet aperiam atque autem
-                            commodi
-                            consectetur, culpa, cupiditate deleniti dolores inventore magnam nulla odit pariatur
-                            perferendis
-                            qui quo rerum tempore velit.
-                        </p>
-                    </div>
-                    <div className={aboutMeStyle.wrapper_box}>
-                        <div>
-                            <div>
-                                <span>1</span>
-                            </div>
-                        </div>
-                        <h3>
-                            Years of Expeariance
-                        </h3>
-                    </div>
+	return (
+		<section className={aboutMeStyle.aboutMe}>
+			<div className={containerStyle.container}>
+				<SectionTItle sectionTitle={'About me'} header={'Know me more'}/>
+				<div className={aboutMeStyle.wrapperBlock1}>
+					<div className={aboutMeStyle.wrapper_description}>
+						<h2>Hi, I'm <span>Alexandr Kuchutkov</span></h2>
+						<p>
+							I'm a frontend developer with a passion for the frontend. I enjoy developing simple, and slick websites and app. My strengths are curious (любопытный), creative, observant (наблюдательный), determined (решительный) and active. I delivering work within time and budget which meets client's requirements is our moto.
+						</p>
+					</div>
+					<div className={aboutMeStyle.wrapper_box}>
+						<div>
+							<div>
+								<span>1</span>
+							</div>
+						</div>
+						<h3>
+							Years of <span>Experiance</span>
+						</h3>
+					</div>
+				</div>
+                <div className={aboutMeStyle.wrapperBlock2}>
+					<AbloutMeBlock title={'Name'} blockText={'Alexandr Kuchutkov'} />
+					<AbloutMeBlock title={'Email'} blockText={'kashim007@mail.ru'} />
+					<AbloutMeBlock title={'Date of birth'} blockText={'14 February, 1991'} />
+					<AbloutMeBlock title={'From'} blockText={'Minsk, Belarus'} />
                 </div>
-            </div>
-        </section>
-    );
+			</div>
+		</section>
+	);
 };
--- src/section/skills/skill/Skill.tsx
+++ src/section/skills/skill/Skill.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import skillStyle from './Skill.module.css'
 
+
 type Props = {
     imgText: string
     title: string
     description: string
+    icon?: any
 };
 export const Skill = (props: Props) => {
     return (
         <div className={skillStyle.skill_container}>
             <div className={skillStyle.icon_wrapper}>
-                {/*<img src="#" alt={props.imgText} className={skillStyle.icon}/>*/}
-                <div></div>
+                {/*<FontAwesomeIcon icon={props.icon} className={skillStyle.icon}/>*/}
+                <img className={skillStyle.icon} src={props.icon} alt=""/>
             </div>
             <h3 className={skillStyle.title}>{props.title}</h3>
             <p className={skillStyle.description}>{props.description}</p>
